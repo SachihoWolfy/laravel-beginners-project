9deb57f5e32001776210098766cd77c993fd9f26
Md Mosharaf Hossan <mdmosharafhossan.swe@gmail.com>
2026-01-28 16:29:44 -0700

    Add player routes and controller

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -2,15 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\Player;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
-
-
     public function index()
     {
-        return "This is a player";
+        return response()->json(Player::all());
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'score' => 'required|integer|min:0',
+        ]);
+
+        $player = Player::create($validated);
+
+        return response()->json($player, 201);
+    }
+
+    public function show(Player $player)
+    {
+        return response()->json($player);
+    }
+
+    public function update(Request $request, Player $player)
+    {
+        $validated = $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'score' => 'sometimes|integer|min:0',
+        ]);
+
+        $player->update($validated);
+
+        return response()->json($player);
+    }
+
+    public function destroy(Player $player)
+    {
+        $player->delete();
+
+        return response()->json(['message' => 'Player deleted']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::get('/players', [PlayerController::class, 'index']);
+Route::post('/players', [PlayerController::class, 'store']);
+Route::get('/players/{player}', [PlayerController::class, 'show']);
+Route::put('/players/{player}', [PlayerController::class, 'update']);
+Route::delete('/players/{player}', [PlayerController::class, 'destroy']);
+
+
+//Route::resource('players', PlayerController::class);
