cdc4eec38bacbcff59d3f81512f6459e3592f8d6
Md Mosharaf Hossan <mdmosharafhossan.swe@gmail.com>
2026-01-28 16:00:53 -0700

    Add player migration, model, seeder, and factory

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+
+
+    public function index()
+    {
+        return "This is a player";
+    }
+}
