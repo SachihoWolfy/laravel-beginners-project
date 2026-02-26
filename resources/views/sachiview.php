df1efc9af9e3097d0c03e4545aea595f4cf00edc
Sachiterasu <37966000+SachihoWolfy@users.noreply.github.com>
2026-02-26 11:50:05 -0700

    Episode 3 partway

---
+++ resources/views/sachiview.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sachi View</title>
+</head>
+<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <nav>
+        <a href="/">Home</a>
+        <a href="/sachi">Sachi</a>
+        <a href="/nexus">Nex</a>  
+    </nav>            
+    <h1>Hello, from Sachi!</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayerWhereController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/sachi', function () {
+    return view('sachiview');
+});
+Route::get('/nexus', function () {
+    return view('nexview');
 });
 
 
